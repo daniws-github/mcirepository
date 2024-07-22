50ccda989d4e94feb6cfc2598cef7b4a7050d914
Abiezaptr <abieza.eresha@gmail.com>
2024-07-22 10:52:14 +0700

    Optimize code

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -18,52 +18,6 @@ class Home extends CI_Controller
         }
     }
 
-    // public function index()
-    // {
-    //     // Ambil semua jenis type
-    //     $query = $this->db->get('type');
-    //     $types = $query->result();
-
-    //     // Ambil user_id dan role dari session
-    //     $user_id = $this->session->userdata('id');
-    //     $role = $this->session->userdata('role');
-
-    //     $documents_by_type = [];
-
-    //     // Loop through each type to get up to 6 latest documents for each type
-    //     foreach ($types as $type) {
-    //         $this->db->select('document.*, type.id as type_id, type.name as type_name, product.id as product_id, product.name as product_name, users.username as upload_name, user_views.last_viewed');
-    //         $this->db->from('document');
-    //         $this->db->join('type', 'type.id = document.type_id', 'left');
-    //         $this->db->join('product', 'product.id = document.product_id', 'left');
-    //         $this->db->join('users', 'users.id = document.user_id', 'left');
-    //         $this->db->join('user_views', 'user_views.document_id = document.id AND user_views.user_id = ' . $user_id, 'left');
-
-    //         if ($role == 2) {
-    //             // Jika role adalah user (2), tambahkan join untuk memeriksa izin pengguna pada dokumen
-    //             $this->db->join('user_document_permissions', 'user_document_permissions.document_id = document.id AND user_document_permissions.user_id = ' . $user_id . ' AND user_document_permissions.status = "open"', 'inner');
-    //         }
-
-    //         $this->db->where('document.type_id', $type->id);
-    //         $this->db->order_by('user_views.last_viewed', 'DESC'); // Urutkan berdasarkan last_viewed secara descending
-    //         $this->db->limit(6); // Tambahkan limit
-    //         $documents_by_type[$type->id] = $this->db->get()->result();
-    //     }
-
-    //     // Siapkan data untuk dikirim ke view
-    //     $data['title'] = 'Homepage';
-    //     $data['types'] = $types;
-    //     $data['documents_by_type'] = $documents_by_type;
-
-    //     $data['logs'] = $this->get_upload_logs();
-    //     $data['read_logs'] = $this->get_user_read_logs();
-
-    //     // Load view dengan data yang sudah disiapkan
-    //     $this->load->view('template/header', $data);
-    //     $this->load->view('home', $data);
-    //     $this->load->view('template/footer');
-    // }
-
     public function index()
     {
         // Ambil semua jenis type
@@ -113,8 +67,6 @@ class Home extends CI_Controller
         $this->load->view('home', $data);
         $this->load->view('template/footer');
     }
-
-
 
     // Method untuk menandai notifikasi sebagai sudah dibaca
     public function readnotif()
