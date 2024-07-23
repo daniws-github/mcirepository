920d849f7a840ba7b52e7a3d6f5132c434c2e482
daniws-github <dani.wika.s@gmail.com>
2024-07-23 14:03:05 +0700

    loginauth

--- application/views/login.php
+++ application/views/login.php
@@ -310,7 +310,7 @@
 				<br>
 				<div class="or"><b>OR</b></div>
 				<br>
-				<a href="https://IP:5000/login" class="google-login">
+				<a href="https://10.49.143.153:5000/authmci" class="google-login">
 					<img src="<?= base_url('assets') ?>/images/microsoft.png" width="20" alt="Google Icon"> Sign in with Microsoft
 				</a>
 			</form>
